cb71d48c2601c5a0ff0bd2a2e59b6cb64b88de17
H0vak <novak.anton2k8@gmail.com>
2026-02-27 19:22:26 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    long long n, m;
+
+    cout << "Введіть два числа (кількість цукерок у Сема і Юри): ";
+    cin >> n >> m;
+
+    long long step = 0;
+
+    while (n > 0 && m > 0) {
+        step++;
+        cout << "Крок " << step << ": Сем їсть 1, Юра їсть 1\n";
+        n--;
+        m--;
+        cout << "Залишилось — Сем: " << n << ", Юра: " << m << "\n\n";
+    }
+
+    cout << "Залишок: " << (n + m) << " цукерок.\n";
+
+    return 0;
+}
